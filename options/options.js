f9b52b7379b5a5acb8cb54f3d613ce8b7604f798
Crowdbotics <engineering@crowdbotics.com>
2023-06-12 08:56:22 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "billowing-forest-de-90701",
+  url: "https://billowing-forest-de-90701.botics.co",
+  api: "https://billowing-forest-de-90701.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
